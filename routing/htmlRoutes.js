0603e3c91313f222bc1b1054bc22d5dd5cd02de7
Kyle Wuellner <rockstar81@comcast.net>
2019-09-04 22:23:07 -0500

    coded survey.html

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 // routing
 module.exports = function (app) {
     app.get("/survey", function (req, res) {
-        res.sendFile.(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));

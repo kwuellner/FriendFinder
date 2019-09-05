b0de5282aa8497f39b7276f7ebbabc8b166c67ce
Kyle Wuellner <rockstar81@comcast.net>
2019-09-04 23:18:07 -0500

    added possible friends

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+let friends = [
+    {
+        name: "Ryan Smith",
+        photo: "https://www.pexels.com/photo/action-adult-adventure-biker-242829/",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "4",
+            "3",
+            "2",
+            "4",
+        ]
+    },
+
+    {
+        name: "Rain Wilson",
+        photo: "https://www.pexels.com/photo/adult-band-close-up-concert-243989/",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "4",
+            "3",
+            "2",
+            "4",
+        ]
+    },
+
+    {
+        name: "Sasha Lang",
+        photo: "https://www.pexels.com/photo/shallow-focus-photo-of-woman-sitting-on-couch-2602085/",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "4",
+            "3",
+            "2",
+            "4",
+        ]
+    },
+
+    {
+        name: "Xavier Barros",
+        photo: "https://www.pexels.com/photo/chef-holding-white-tea-cup-887827/",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "4",
+            "3",
+            "2",
+            "4",
+        ]
+    }
+];
+
+modal_exports = friends;
